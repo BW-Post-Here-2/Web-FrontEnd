e493f796adf8a6348190c3ca036ce275d955189a
Drew Ross <mail.drew.ross@gmail.com>
2020-05-29 10:17:17 -0700

    Update yup form validation to use regular expressions to forbid spaces or special characters in the username, and spaces in the password.

--- src/Components/Auth/validation/formSchema.js
+++ src/Components/Auth/validation/formSchema.js
@@ -1,10 +1,16 @@
 import * as yup from 'yup';
 
+//
+const userRegex = '^[\\w]+$'
+const passRegex = '^[\\S]+$'
+
 const formSchema = yup.object().shape({
-    username: yup.string().trim()
+    username: yup.string()
+        .matches(userRegex, 'Username must not have spaces or special characters.')
         .min(2, 'Username must be at least 2 characters.')
         .required('Username required.'),
-    password: yup.string().trim()
+    password: yup.string()
+        .matches(passRegex, 'Password must not have spaces.')
         .min(8, 'Password must be at least 8 characters.')
         .required('Password required.'),
 
